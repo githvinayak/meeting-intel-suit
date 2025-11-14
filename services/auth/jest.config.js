5eaa1311ebb4cefcdaabea1c1b6393853983c3b0
githvinayak <vinayak192002@gmail.com>
2025-11-14 18:13:02 +0530

    created jest.config.js

---
+++ services/auth/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__test__/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/__tests__/**',
+    '!src/__test__/**',
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  testTimeout: 10000,
+  clearMocks: true,
+};
