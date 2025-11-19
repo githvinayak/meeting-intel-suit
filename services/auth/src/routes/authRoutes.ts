35d4dd29ca1f935529966877ee1268fbf817229f
Vinayak <dollarvinayak@gmail.com>
2025-11-20 00:13:34 +0530

    added login route for testing login controller

--- services/auth/src/routes/authRoutes.ts
+++ services/auth/src/routes/authRoutes.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
-import { register } from '../controllers/authController';
+import { login, register } from '../controllers/authController';
+import { authenticate } from '../middleware/authenticate';
 
 const router: Router = express.Router();
 
@@ -11,5 +12,15 @@ router.use((req, _res, next) => {
 
 // POST /api/v1/auth/register
 router.post('/register', register);
+router.post('/login', login);
+router.get('/me', authenticate, (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'User retrieved successfully',
+    data: {
+      user: req.user,
+    },
+  });
+});
 
 export default router;
